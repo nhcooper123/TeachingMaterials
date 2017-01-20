debecb3b985e212ee4e9bf07ba6d7c32f96c1d60
Natalie Cooper <ncooper@tcd.ie>
2017-01-20 17:33:13 +0000

    Initial commit - how to complie gitbook

---
+++ MSc_Museum/CompilingGitBook.R
@@ -0,0 +1,6 @@
+setwd("~/Projects/TeachingMaterials/MSc_Museum/MacroModule")
+bookdown::render_book("index.Rmd")
+
+# Need to remove numbering from headers
+# Remove block quotes
+# Sort out HTML version
