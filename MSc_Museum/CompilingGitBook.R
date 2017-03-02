f00f4595c0687cce2a974a34930923164fd9683e
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2017-03-02 16:20:45 +0000

    Transfer to new repo

--- MSc_Museum/CompilingGitBook.R
+++
@@ -1,7 +0,0 @@
-# WORKS IN TERMINAL, NOT RSTUDIO OR RGUI
-
-r
-setwd("~/Projects/TeachingMaterials/MSc_Museum/MacroModule")
-bookdown::render_book("index.Rmd")
-bookdown::render_book("index.Rmd", "bookdown::gitbook") # HTML version
-bookdown::render_book("index.Rmd", "bookdown::pdf_book") # PDF version
