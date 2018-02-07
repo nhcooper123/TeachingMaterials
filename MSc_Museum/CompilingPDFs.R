537d2ab8205e4969e0ec5d1719910b83ede6c26e
Natalie Cooper <ncooper@tcd.ie>
2018-02-07 16:11:02 +0000

    Minor updates for 2018

--- MSc_Museum/CompilingPDFs.R
+++ MSc_Museum/CompilingPDFs.R
@@ -5,6 +5,7 @@ https://github.com/astefanutti/decktape#install
 
 # Run the following code for each lecture
 cd decktape-1.0.0
+
 ./phantomjs decktape.js slidy -s 1024x720 -p 4000 https://rawgit.com/nhcooper123/TeachingMaterials/master/MSc_Museum/Lectures/intro.html intro.pdf
 
 # -s sets the output viewport size
